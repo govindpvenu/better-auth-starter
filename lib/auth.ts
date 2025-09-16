d04773c48c1cb9a5ba6d7ceaef60214693005b85
govindpvenu <govindpvenu.txt@gmail.com>
2025-09-16 16:15:55 +0530

    feat: google auth; account linking

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,6 +22,16 @@ export const auth = betterAuth({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+    },
   },
   plugins: [
     emailOTP({
