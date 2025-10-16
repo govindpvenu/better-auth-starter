7833d48d42e0b3d07e4e11dbb522c1848cc42a83
govindpvenu <govindpvenu.txt@gmail.com>
2025-10-16 15:14:34 +0530

    fix: auth providers not working after extending core schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,11 +54,23 @@ export const auth = betterAuth({
         github: {
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+            mapProfileToUser: (profile) => {
+                return {
+                    firstName: profile.name.split(' ')[0],
+                    lastName: profile.name.split(' ')[1],
+                };
+            },
         },
         google: {
             prompt: 'select_account',
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            mapProfileToUser: (profile) => {
+                return {
+                    firstName: profile.given_name,
+                    lastName: profile.family_name,
+                };
+            },
         },
     },
     account: {
